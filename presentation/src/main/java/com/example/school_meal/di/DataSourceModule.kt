882496f2bd65598f391c2f03871542a078ffda8b
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-30 23:14:31 +0900

    [7월 30일] Module 세팅

---
+++ presentation/src/main/java/com/example/school_meal/di/DataSourceModule.kt
@@ -0,0 +1,10 @@
+package com.example.school_meal.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataSourceModule {
+}

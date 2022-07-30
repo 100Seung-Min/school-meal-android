9944001a09a1a8960f424c13a17931926adfd2b8
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-30 23:07:03 +0900

    [7월 30일] setting.gradle 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
     }
 }
 rootProject.name = "School_Meal"
-include ':presentation'
-include ':domain'
-include ':data'
+include(":presentation")
+include(":domain")
+include(":data")

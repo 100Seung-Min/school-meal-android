f5469275089816f884a033c8672d573fae0d53d8
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-08 09:24:31 +0900

    [8월 8일] MealResponse Mapper

--- data/src/main/java/com/example/data/remote/response/MealResponse.kt
+++ data/src/main/java/com/example/data/remote/response/MealResponse.kt
@@ -1,5 +1,6 @@
 package com.example.data.remote.response
 
+import com.example.domain.entity.MealEntity
 import com.google.gson.annotations.SerializedName
 
 data class MealResponse(
@@ -18,5 +19,19 @@ data class MealResponse(
             @SerializedName("MMEAL_SC_NM")
             val mealTime: String
         )
+
+        fun DietRow.toEntity() = MealEntity.MealServiceDietInfo.DietRow(
+            dishName = dishName,
+            mealDay = mealDay,
+            mealTime = mealTime
+        )
     }
+
+    fun MealServiceDietInfo.toEntity() = MealEntity.MealServiceDietInfo(
+        row = row.map { it.toEntity() }
+    )
 }
+
+fun MealResponse.toEntity() = MealEntity(
+    mealServiceDietInfo = mealServiceDietInfo.map { it.toEntity() }
+)

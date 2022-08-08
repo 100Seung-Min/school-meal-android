eb4a1d9c3d9df654ab4cb0854ff017784a870ca3
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-08 09:40:04 +0900

    [8월 8일] TimeRepository

--- presentation/src/main/java/com/example/school_meal/di/RepositoryModule.kt
+++ presentation/src/main/java/com/example/school_meal/di/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.example.school_meal.di
 
 import com.example.data.remote.datasource.MealDataSourceImpl
 import com.example.data.remote.datasource.SchoolDataSourceImpl
+import com.example.data.remote.datasource.TimeDataSourceImpl
 import com.example.data.repository.MealRepositoryImpl
 import com.example.data.repository.SchoolRepositoryImpl
+import com.example.data.repository.TimeRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,4 +22,8 @@ object RepositoryModule {
     @Provides
     @Singleton
     fun provideSchoolRepository(dataSource: SchoolDataSourceImpl) = SchoolRepositoryImpl(dataSource)
+
+    @Provides
+    @Singleton
+    fun provideTimeRepository(dataSource: TimeDataSourceImpl) = TimeRepositoryImpl(dataSource)
 }

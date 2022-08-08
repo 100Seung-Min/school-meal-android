29bdb2da06da326fcf8613e07a3be86d668ba520
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-08 17:00:11 +0900

    [8월 8일] SearchSchoolAdapter 수정

--- presentation/src/main/java/com/example/school_meal/ui/adapter/SearchSchoolAdapter.kt
+++ presentation/src/main/java/com/example/school_meal/ui/adapter/SearchSchoolAdapter.kt
@@ -2,19 +2,20 @@ package com.example.school_meal.ui.adapter
 
 import android.content.Context
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.data.remote.response.infoRow
 import com.example.school_meal.R
+import com.example.domain.entity.SchoolEntity.SchoolInfo.InfoRow
 
-class SearchSchoolAdapter(val itemlist: ArrayList<infoRow>, val context: Context, val itemClick : (infoRow) -> Unit): RecyclerView.Adapter<SearchSchoolAdapter.ViewHolder>() {
+class SearchSchoolAdapter(val itemlist: List<InfoRow>?, val itemClick : (InfoRow) -> Unit): RecyclerView.Adapter<SearchSchoolAdapter.ViewHolder>() {
     class ViewHolder(v: View): RecyclerView.ViewHolder(v) {
-        fun bind(item: infoRow, context: Context, itemClick : (infoRow) -> Unit){
+        fun bind(item: InfoRow, itemClick : (InfoRow) -> Unit){
             itemView.findViewById<TextView>(R.id.find_school_text).setBackgroundColor(Color.GRAY)
-            itemView.findViewById<TextView>(R.id.find_school_text).text = item.SCHUL_NM
+            itemView.findViewById<TextView>(R.id.find_school_text).text = item.schoolName
             itemView.findViewById<TextView>(R.id.find_school_text).setOnClickListener {
                 itemClick(item)
                 itemView.findViewById<TextView>(R.id.find_school_text).setBackgroundColor(Color.GREEN)
@@ -28,10 +29,10 @@ class SearchSchoolAdapter(val itemlist: ArrayList<infoRow>, val context: Context
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(itemlist[position], context, itemClick)
+        holder.bind(itemlist!![position], itemClick)
     }
 
     override fun getItemCount(): Int {
-        return itemlist.size
+        return itemlist?.size ?: 0
     }
 }

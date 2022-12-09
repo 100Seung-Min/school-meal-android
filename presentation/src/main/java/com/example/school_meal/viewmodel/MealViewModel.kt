1c062694495280b8292e16f30f2569c3a02c3b86
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-09 09:30:40 +0900

    급식 달 변경 기능 구현

--- presentation/src/main/java/com/example/school_meal/viewmodel/MealViewModel.kt
+++ presentation/src/main/java/com/example/school_meal/viewmodel/MealViewModel.kt
@@ -9,6 +9,9 @@ import com.example.domain.usecase.school.SchoolMealUseCase
 import com.example.school_meal.ui.extension.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,19 +22,33 @@ class MealViewModel @Inject constructor(
     val mealInfo: MutableLiveData<List<MealEntity.MealItem>> get() = _mealInfo
     private val _currentMeal = SingleLiveEvent<String>()
     val currentMeal: MutableLiveData<String> get() = _currentMeal
+    private val _currentMonth = SingleLiveEvent<String>()
+    val currentMonth: MutableLiveData<String> get() = _currentMonth
 
     init {
         _currentMeal.value = "조식"
+        _currentMonth.value = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
     }
 
-    fun meal(day: String) = viewModelScope.launch {
+    fun meal() = viewModelScope.launch {
         kotlin.runCatching {
-            schoolMealUseCase.execute(day)
+            schoolMealUseCase.execute(_currentMonth.value!!.slice(0..5))
         }.onSuccess {
             _mealInfo.value = it?.row
         }.onFailure {
 
         }
+    }
+
+    fun setMonth(isPlus: Boolean = true) {
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        var date = LocalDate.parse(_currentMonth.value, formatter)
+        date = if (isPlus) {
+            date.plusMonths(1)
+        } else {
+            date.minusMonths(1)
+        }
+        _currentMonth.value = date.format(formatter)
     }
 
     fun setMealType(type: String) {

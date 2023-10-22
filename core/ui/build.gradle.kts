adfb16cb80637a73e0a4086dca228200f1af86c0
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-10-23 08:32:31 +0900

    ui 모듈 세팅

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("school-core")
+}
+
+android {
+    namespace = "com.school.core.ui"
+}
+
+dependencies {
+    implementation(project(":core:domain"))
+    implementation(libs.bundles.compose)
+    implementation(libs.coil)
+    implementation(libs.bundles.retrofit)
+}

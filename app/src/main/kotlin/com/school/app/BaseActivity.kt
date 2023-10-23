8edad568f34de9e15797551f00be2bba229ed3c3
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-10-23 09:00:52 +0900

    MainActivity 세팅

---
+++ app/src/main/kotlin/com/school/app/BaseActivity.kt
@@ -0,0 +1,55 @@
+package com.school.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.school.core.design_system.SchoolTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
+
+    @OptIn(ExperimentalAnimationApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            navController = rememberAnimatedNavController()
+            SchoolTheme {
+                BaseApp(navController = navController)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun BaseApp(navController: NavHostController) {
+    AnimatedNavHost(
+        navController = navController,
+        startDestination = "",
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { it * 2 }, animationSpec = tween(
+                    durationMillis = 500
+                )
+            )
+        },
+        popEnterTransition = { fadeIn(animationSpec = tween(durationMillis = 500)) },
+        popExitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { it * 2 }, animationSpec = tween(
+                    durationMillis = 500
+                )
+            )
+        }
+    ) {
+    }
+}
